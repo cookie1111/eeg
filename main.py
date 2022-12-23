ebfa8514590e3a53d11fd6c6aa4da239422df626
Sebastjan <sebastjan.kramar@gmail.com>
2022-12-23 14:51:33 +0100

    Testing out ICA

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from mne.io import read_raw_eeglab
+from mne.preprocessing import ICA
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    a = read_raw_eeglab("/home/sebastjan/Downloads/ds003490-download/sub-001/ses-01/eeg/sub-001_ses-01_task-Rest_eeg.set")
-    print("loaded")
-    #a.plot(block=False)
-    channel = a[0,:]
-    #plt.ion()
+    a = read_raw_eeglab("ds003490-download/sub-007/ses-01/eeg/sub-007_ses-01_task-Rest_eeg.set")
+    a.plot()
+
+
+    # Pergorming ICA on the raw signal
+    # guidelines below:
+    # https://sccn.ucsd.edu/wiki/Makoto%27s_preprocessing_pipeline#General_tips_for_performing_ICA_.2806.2F26.2F2018_updated.29
+    ic = ICA(10)
+    ic.fit(a)
+
+    ic.plot_sources(a)
+
+    print("stand")
