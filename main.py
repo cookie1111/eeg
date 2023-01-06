d61f91d120826c8b384711f996597b714c72ad83
Sebastjan <sebastjan.kramar@gmail.com>
2023-01-06 11:24:35 +0100

    checking the dataset

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import fastica
 
+# prve  5 min je rest state z označenim ko so oči odprte in ko so oči zaprte. I guess da referenca??
+# pol so pa toni ki se spreminjajo iz sekunde v sekundo, 70% procentov je 440Hz sinusni ton, 15% je 660Hz  sinusni ton,ž
+# 15% je naturalistični toni iz zvočnega dataseta.
+
+# zapiranje in odpiranje oči je zelo opazno v EEG posnetku.
+
+
 if __name__ == '__main__':
-    a = read_raw_eeglab("ds003490-download/sub-007/ses-01/eeg/sub-007_ses-01_task-Rest_eeg.set")
+    a = read_raw_eeglab("/home/sebastjan/PycharmProjects/eeg/ds003490-download/sub-050/ses-01/eeg/sub-050_ses-01_task-Rest_eeg.set")
     a.plot()
 
 
