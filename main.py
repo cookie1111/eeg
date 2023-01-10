80ec871506077bd11cc91dc37ea0373234a65b3e
Sebastjan <sebastjan.kramar@gmail.com>
2023-01-10 18:05:19 +0100

    a few more articles

--- main.py
+++ main.py
@@ -12,19 +12,23 @@ from sklearn.decomposition import fastica
 
 
 if __name__ == '__main__':
-    #a = read_raw_eeglab("/home/sebastjan/PycharmProjects/eeg/ds003490-download/sub-050/ses-01/eeg/sub-050_ses-01_task-Rest_eeg.set")
-    a = read_raw_brainvision("/home/sebastjan/PycharmProjects/eeg/eeg/Control1025.vhdr")
+    a = read_raw_eeglab("/home/sebastjan/PycharmProjects/eeg/ds003490-download/sub-050/ses-01/eeg/sub-050_ses-01_task-Rest_eeg.set")
+    #a = read_raw_brainvision("/home/sebastjan/PycharmProjects/eeg/eeg/Control1025.vhdr")
     a.plot()
 
 
     # Pergorming ICA on the raw signal
     # guidelines below:
     # https://sccn.ucsd.edu/wiki/Makoto%27s_preprocessing_pipeline#General_tips_for_performing_ICA_.2806.2F26.2F2018_updated.29
-    ic = ICA(60)
+    ic = ICA(30)
     ic.fit(a)
 
-    ic.plot_sources(a)
 
-    df = ic.to_data_frame()
+    ic.plot_sources(a)
+    ic.plot_components()
+
+    df = a.to_data_frame()
+
+    print(df.head)
 
     print("stand")
