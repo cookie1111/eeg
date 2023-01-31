305684b027158b3b1486c850f2a8eb09c2efcecf
Sebastjan <sebastjan.kramar@gmail.com>
2023-01-31 12:21:50 +0100

    need to preproccess the raw correctly for the epochs to not be rejected!!

--- eeg_preproc.py
+++ eeg_preproc.py
@@ -102,36 +102,41 @@ class EEGDataset(Dataset):
                 eeg_file = os.path.join(subject_path_eeg,
                                         [f for f in os.listdir(subject_path_eeg) if f.endswith('.set')][0])
                 raw = mne.io.read_raw_eeglab(eeg_file, preload=True)
+                low_cut = 0.1
+                hi_cut = 30
+                raw.filter(low_cut, hi_cut)
                 raw.crop(tmin=self.tstart, tmax=self.tend)
 
                 if not self.special_part:
                     rest_events = mne.make_fixed_length_events(raw, id=1, duration=2, overlap=1.9)
                     print(self.tstart,self.tend)
-                    save_dest = os.path.join(subject_path_eeg, f"{self.medicated}_{self.tstart}_{self.tend}_epo.fif")
+                    save_dest = os.path.join(subject_path_eeg, f"{self.medicated}_{self.tstart}_{self.tend}_noDrop_epo.fif")
                     if len(self.cache) < self.cache_size:
-                        if os.path.isfile(save_dest):
-                            rest_epochs = mne.read_epochs(save_dest)
-                        else:
-                            rest_epochs = mne.Epochs(raw,
-                                                     rest_events,
-                                                     1,
-                                                     self.tstart,
-                                                     self.tend,
-                                                     baseline=None).drop_bad()
-
-                            rest_epochs.save(save_dest)
-                        # have index saved for easier cache checking
-                        self.cache.append((len(rest_epochs)+0 if len(self.cache) == 0 else self.cache[-1][0],
-                                           rest_epochs))
-
-                        self.epochs_list.append(save_dest)
-                    else:
+                        #if os.path.isfile(save_dest):
+                        #    rest_epochs = mne.read_epochs(save_dest)
+                        #else:
                         rest_epochs = mne.Epochs(raw,
                                                  rest_events,
                                                  1,
                                                  self.tstart,
                                                  self.tend,
-                                                 baseline=None).drop_bad()
+                                                 baseline=(None, None)).drop_bad()
+                        print(f"________WRITING TO {save_dest}________")
+                        rest_epochs.save(save_dest,overwrite=True)
+                        # have index saved for easier cache checking
+                        self.cache.append((len(rest_epochs)+0 if len(self.cache) == 0 else self.cache[-1][0],
+                                           rest_epochs))
+
+                        self.epochs_list.append(save_dest)
+                        rest_epochs = mne.Epochs(raw,
+                                                 rest_events,
+                                                 1,
+                                                 self.tstart,
+                                                 self.tend,
+                                                 baseline=(None, None)).drop_bad()
+                    else:
+                        rest_epochs.save(save_dest, overwrite=True)
+                        self.epochs_list.append(save_dest)
                     #mne.save(os.path.join(subject_path,'saved_epoch.fif', overwrite=True), rest_epochs)
             else:
                 eeg_file1 = os.path.join(subject_path1, [f for f in os.listdir(subject_path1) if f.endswith('.set')][0])
