bd5829b162b899a024c1ce3b2a3125d54ab2676a
Sebastjan <sebastjan.kramar@gmail.com>
2023-02-17 16:34:09 +0100

    implemented Resnet with blocks need to figure out how to use weights and then to transform input usign cwt spectrogram

--- ResNet.py
+++ ResNet.py
@@ -10,6 +10,95 @@ from eeg_preproc import EEGDataset
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+class ResBlock(nn.Module):
+    def __init__(self, input_size, output_size, stride = 1,expansion = 1, downsample = None):
+        super(ResBlock, self).__init__()
+
+        # depanding on the size of the model some have expansion from one conv layer
+        # to the next within a block
+        self.expansion = expansion
+        self.downsample = downsample
+
+        self.conv1 = nn.Conv2d(input_size, output_size, kernel_size=3,stride=stride,padding=1,bias=False)
+        self.bn1 = nn.BatchNorm2d(output_size)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(output_size,output_size*expansion, kernel_size=3, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(output_size*expansion)
+
+    def forward(self, x):
+        # this puts the residual in residual network
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        oout = out + identity
+        out = self.relu(out)
+        return out
+
+class ResNet18WithBlocks(nn.Module):
+
+    def __init__(self, img_channels, num_layers, block, num_classes = 2):
+        super(ResNet18WithBlocks, self).__init__()
+        if num_layers == 18:
+
+            layers = [2,2,2,2]
+            self.expansion = 1
+
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(in_channels=img_channels,out_channels=self.in_channels, kernel_size = 7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channels)
+        self.relu = nn. ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(512*self.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, blocks, stride = 1):
+        downsample = None
+        if stride != 1:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels*self.expansion, kernel_size=1,stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels * self.expansion))
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, self.expansion, downsample))
+        self.in_channels = out_channels * self.expansion
+
+        for i in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels, expansion=self.expansion))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        print(x.type())
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x,1)
+        x = self.fc(x)
+
+        return x
+
 class ResNet18(nn.Module):
 
     def __init__(self, num_classes, num_last_layers):
@@ -43,6 +132,7 @@ def train(model, dloader_train, optimizer, criterion,):
     a = len(dloader_train)
     i = 0
     for ins, labels in dloader_train:
+        ins = ins.double()
         optimizer.zero_grad()
 
         outs = model(ins)
@@ -55,10 +145,13 @@ def train(model, dloader_train, optimizer, criterion,):
         i = i + 1
 
 if __name__ == "__main__":
-    res = ResNet18(2,2)
+    res = ResNet18WithBlocks(1,18,ResBlock)
+    res = res.double()
+    #res = ResNet18(2,2)
     dset = EEGDataset("./ds003490-download", participants="participants.tsv",
                                   tstart=0, tend=240, cache_amount=1, batch_size=8, )
     dtrain, dtest = dset.split(ratios=0.9)
+    del dset
     optimizer = optim.SGD(res.parameters(), lr= 0.01, momentum=0.9)
     criterion = nn.CrossEntropyLoss()
     train(res,DataLoader(dtest, batch_size=8,shuffle=False,num_workers=1),optimizer, criterion)
